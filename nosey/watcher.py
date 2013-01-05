c2e9d7ec7c33d2147b828e542023eeec549a7d27
Joe Esposito <joe@joeyespo.com>
2013-01-05 15:56:00 -0500

    Implement basic watchdog handler.

--- nosey/watcher.py
+++ nosey/watcher.py
@@ -1,4 +1,26 @@
 import os
+import time
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class ChangeHandler(FileSystemEventHandler):
+    """Listens for changes to files and re-runs tests after each change."""
+    def __init__(self, directory=None):
+        super(ChangeHandler, self).__init__()
+        self.directory = os.path.abspath(directory or '.')
+
+    def on_any_event(self, event):
+        if event.is_directory:
+            return
+        self.run()
+
+    def run(self):
+        """Called when a file is changed to re-run the tests with nose."""
+        print
+        print 'Running unit tests...'
+        subprocess.call('nosetests', cwd=self.directory)
 
 
 def watch(directory=None):
@@ -8,6 +30,14 @@ def watch(directory=None):
     directory = os.path.abspath(directory)
 
     print ' * Watching', directory
-
-    # TODO: implement
-    raise NotImplementedError()
+    event_handler = ChangeHandler()
+    event_handler.run()
+    observer = Observer()
+    observer.schedule(event_handler, path=directory, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
