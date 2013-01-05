491787832d32d360ba93cfb9ab84f79c92b9c7c6
Joe Esposito <joe@joeyespo.com>
2013-01-05 17:04:24 -0500

    Set version to 0.1.1.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def read(fname):
 
 setup(
     name='nosey',
-    version='0.1.0',
+    version='0.1.1',
     description='Local continuous test runner with nose and watchdog.',
     long_description=__doc__,
     author='Joe Esposito',
